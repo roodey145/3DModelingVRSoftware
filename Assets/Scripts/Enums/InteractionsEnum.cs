f42df9cb167db2817b3707426e099a02b208a635
roodey145 <abdu2624@outlook.com>
2023-04-14 16:46:46 +0200

    Sounds script has been added

--- Assets/Scripts/Enums/InteractionsEnum.cs
+++ Assets/Scripts/Enums/InteractionsEnum.cs
@@ -1,4 +1,4 @@
-public enum InteractionsEnum 
+public enum InteractionsEnum : byte
 {
     ScaleUp = 0,
     ScaleDown,
@@ -9,4 +9,35 @@ public enum InteractionsEnum
     LoopCut,
     Bevel,
     Confirm
+}
+
+
+public static class InteractionsEnumExtensions
+{
+    public static string name(this InteractionsEnum interactionsEnum)
+    {
+        switch (interactionsEnum)
+        {
+            case InteractionsEnum.ScaleUp:
+                return "ScaleUp";
+            case InteractionsEnum.ScaleDown:
+                return "ScaleDown";
+            case InteractionsEnum.Grab:
+                return "Grab";
+            case InteractionsEnum.Move:
+                return "Move";
+            case InteractionsEnum.Rotate:
+                return "Rotate";
+            case InteractionsEnum.Extrude:
+                return "Extrude";
+            case InteractionsEnum.LoopCut:
+                return "LoopCut";
+            case InteractionsEnum.Bevel:
+                return "Bevel";
+            case InteractionsEnum.Confirm:
+                return "Confirm";
+            default:
+                return "";
+        }
+    }
 }
---
+++ Assets/Scripts/Sound/Sounds.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour
+{
+    public enum Scene
+    {
+        
+    }
+
+    private static AudioClip[] interactionSounds;
+    private static AudioClip[] backgroundSounds;
+    // Start is called before the first frame update
+
+
+    private void Awake()
+    {
+        // Loading the audio from the resources folder
+        interactionSounds = Resources.LoadAll<AudioClip>("Sounds/Interaction");
+        backgroundSounds = Resources.LoadAll<AudioClip>("Sounds/Background");
+
+    }
+
+    /// <summary>
+    /// Uses the interaction type to retrieve its sound.
+    /// </summary>
+    /// <param name="InteractionEnum">The interaction type.</param>
+    /// <returns>The sound which shall play.</returns>
+    public static AudioClip GetInteractionSoundClip(InteractionsEnum InteractionEnum)
+    {
+        //AudioClip audio = _GetInteractionAudioClip(InteractionEnum.name());
+        // Incase we want to edit the audio, we can do it here
+        return _GetInteractionAudioClip(InteractionEnum.name());
+    }
+    public static AudioClip GetBackgroundSoundClip(Scene SceneEnum)
+    {
+        AudioClip audio = null;
+        switch (SceneEnum)
+        {
+
+            /*case Scene.textHere:
+                return interactionSounds[x];
+                break;
+            */
+            default:
+                audio = backgroundSounds[0];
+                break;
+        } 
+        return audio;
+    }
+
+
+    // Internal help functions
+
+    private static AudioClip _GetInteractionAudioClip(string audioName)
+    {
+        AudioClip audio = null;
+
+        foreach(AudioClip clip in interactionSounds)
+        {
+            if(clip.name == audioName)
+            { // The target audio has been found
+                audio = clip;
+                break;
+            }
+        }
+
+        return audio;
+    }
+
+}
