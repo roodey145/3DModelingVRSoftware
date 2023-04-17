53177ee73f6757ab3339ede0ce1d2adca6fa161a
roodey145 <abdu2624@outlook.com>
2023-04-17 14:44:53 +0200

    Working on the face script

---
+++ Assets/Scripts/Enums/Direction.cs
@@ -0,0 +1,11 @@
+public enum Direction : int
+{
+    top = 0,
+    right = 1,
+    bottom = 2,
+    left = 3,
+    
+    verctial,
+    horizontal,
+    all
+}
---
+++ Assets/Scripts/ModelsData/Face.cs
@@ -0,0 +1,329 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script is intended to get the data of four points and create a face out of them.
+/// The Face class does not actually have access to the vertices position, but to the 
+/// indeces of the vertices, thus, to get access to the vertices coordinate, the vertices
+/// array in the model class shall be used. 
+/// </summary>
+
+public class Face
+{
+    private enum VerticesPos : int
+    {
+        topLeft = 0,
+        topRight = 1,
+        bottomRight = 2,
+        bottomLeft = 3
+    }
+
+    private int[] _verticesIndex; // TopLeft, topRight, bottomRight, bottomLeft
+    private Vector2Int[] _lines;
+    private Face[] _faces; // Neighbour faces
+
+    private static byte _linesCount = 4;
+    private static byte _facesCount = 4;
+
+    /// <summary>
+    /// An empty face that has data. The faces and lines of this face shall be assigned
+    /// manually
+    /// </summary>
+    public Face()
+    {
+        _lines = new Vector2Int[_linesCount];
+        _faces = new Face[_facesCount];
+    }
+
+
+    public Face(Face[] neighbourFaces, int[] verticesIndex)
+    {
+        // Make sure there is enough faces and lines
+        if (neighbourFaces.Length < _facesCount)
+        {
+            throw new Exception($"Expecting Faces array of length {_facesCount}, " +
+                                $"but got a Faces array of length {neighbourFaces.Length}");
+        }
+        if (verticesIndex.Length < _linesCount)
+        {
+            throw new Exception($"Expecting int[] array of length {_linesCount}, " +
+                                $"but got a int[] array of length {verticesIndex.Length}");
+        }
+        _faces = neighbourFaces;
+        _verticesIndex = verticesIndex;
+    }
+
+    /// <summary>
+    /// Create a face.
+    /// </summary>
+    /// <param name="neighbourFaces">
+    /// The faces which are connected to this one, ordered as follows:
+    ///     Top, right, bottom, left.
+    /// </param>
+    /// <param name="lines">
+    /// The lines of this face ordered as follows:
+    ///     Top, right, bottom, left.
+    /// </param>
+    public Face(Face[] neighbourFaces, Vector2Int[] lines)
+    {
+        // Make sure there is enough faces and lines
+        if(neighbourFaces.Length < _facesCount)
+        {
+            throw new Exception($"Expecting Faces array of length {_facesCount}, " +
+                                $"but got a Faces array of length {neighbourFaces.Length}");
+        }
+        if(lines.Length < _linesCount)
+        {
+            throw new Exception($"Expecting Vector2Int array of length {_linesCount}, " +
+                                $"but got a Vector2Int array of length {lines.Length}");
+        }
+        _faces = neighbourFaces;
+        _lines = lines;
+    }
+
+    public Vector2Int[] GetLines(Direction dir)
+    {
+        Vector2Int[] lines = null;
+        switch(dir)
+        {
+            case Direction.horizontal:
+                lines = new Vector2Int[]
+                {
+                    _lines[(int)Direction.left], _lines[(int)Direction.right]
+                };
+                break;
+
+            case Direction.verctial:
+                lines = new Vector2Int[]
+                {
+                    _lines[(int)Direction.top], _lines[(int)Direction.bottom]
+                };
+                break;
+
+            case Direction.all:
+                lines = (Vector2Int[])_lines.Clone();
+                break;
+        }
+
+        return lines;
+    }
+
+    /// <summary>
+    /// Get a neighbour face using one of the following directions only:
+    ///     top, right, bottom, left
+    /// </summary>
+    /// <param name="dir">The direction of the face to be fetched.</param>
+    /// <returns>Return the face connected to this face in the specified direction.</returns>
+    public Face GetNeighbourFace(Direction dir)
+    {
+        Face face;
+        
+        // The reason this if statement works is, the face that the expected directions
+            // top, right, bottom, left has the numerical values 0, 1, 2, 3 respectively. 
+        if((int)dir < _facesCount)
+        {
+            face = _faces[(int)dir];
+        }
+        else
+        {
+            throw new Exception($"Expected top, right, bottom, left directions, " +
+                                $"but got {dir}");
+        }
+
+        return face;
+    }
+
+
+
+    public void SetVerticesIndex(int[] verticesIndex)
+    {
+        if(verticesIndex.Length == _linesCount)
+        {
+            _verticesIndex = verticesIndex;
+        }
+    }
+
+    /// <summary>
+    /// Assign a line to this face.
+    /// </summary>
+    /// <param name="line">
+    /// A vector2int that contains the indeces of the vertices which connects this line.
+    /// </param>
+    /// <param name="dir">
+    /// Where the line shall be placed. 
+    /// Expected values: top, right, bottom, left
+    /// </param>
+    public void SetLine(Vector2Int line, Direction dir)
+    {
+        if((int)dir < _linesCount)
+        {
+            _lines[(int)dir] = line;
+        }
+    }
+
+    /// <summary>
+    /// Assign a line to this face.
+    /// </summary>
+    /// <param name="face">
+    /// A face that shares a line with this face.
+    /// </param>
+    /// <param name="dir">
+    /// Where the face shall be placed. 
+    /// Expected values: top, right, bottom, left
+    /// </param>
+    public void SetFace(Face face, Direction dir)
+    {
+        if ((int)dir < _facesCount)
+        {
+            _faces[(int)dir] = face;
+        }
+    }
+
+    /// <summary>
+    /// Takes the indeces of two points and uses them to split this face in two.
+    /// It is expected that the index of the first vertix shall match the right/top edge
+    /// and the index of the second vertix shall match the left/bottom edge.
+    /// </summary>
+    /// <param name="verticesIndex">
+    /// The index of the vertices which are stored in the model's vertices array.
+    /// </param>
+    /// <param name="splitPlacement">
+    /// The placement of the split between the vertices. Value between zero and one. 
+    /// A value of 0.01 means one of the new two faces will have 1% of the original
+    /// face size and the other will have 99%. A value of 0.5 means the new faces 
+    /// will be identical since the split will happen in the center.
+    /// </param>
+    /// <param name="dir">
+    /// The splitting direction. Expected values are: verticle or horizontal
+    /// </param>
+    /// <returns>Two faces that are connected to the rest of the model.</returns>
+    public Face[] Split(Vector2Int verticesIndex, float splitPlacement, Direction dir)
+    {
+        Face[] faces = null;
+
+        Vector2Int[] linesToCut = null;
+
+        if(dir == Direction.verctial)
+        {
+            faces = _cutVerticlly(verticesIndex);
+        }
+        else if(dir == Direction.horizontal)
+        {
+            linesToCut = new Vector2Int[]
+            {
+                _lines[(int)Direction.top],
+                _lines[(int)Direction.bottom]
+            };
+        }
+        else
+        {
+            throw new Exception($"Expected vertical or horizontal directions, " +
+                                $"but got {dir}");
+        }
+
+
+
+
+        return faces;
+    }
+
+
+    private Face[] _cutVerticlly(Vector2Int verticesIndex)
+    {
+        Face[] faces = new Face[2];
+
+        //-------------------------------------------
+        // Create the Left face
+        faces[0] = new Face();
+
+        int[] leftFaceVerticesIndex = new int[]
+        {
+            verticesIndex.x, // TopLeft
+            _verticesIndex[(int)VerticesPos.topRight], // TopRight
+            _verticesIndex[(int)VerticesPos.bottomRight], // BottomRight
+            verticesIndex.y, // BottomLeft
+        };
+
+        faces[1].SetVerticesIndex(leftFaceVerticesIndex);
+
+        //// Create and Set the left line
+        //Vector2Int leftFaceLeftLine = new Vector2Int();
+        //leftFaceLeftLine.x = _lines[(int)Direction.top].x;
+        //leftFaceLeftLine.y = _lines[(int)Direction.bottom].x;
+        //faces[0].SetLine(leftFaceLeftLine, Direction.left);
+
+        //// Create and Set the top line
+        //Vector2Int leftFaceTopLine = new Vector2Int();
+        //leftFaceTopLine.x = _lines[(int)Direction.top].x;
+        //leftFaceTopLine.y = verticesIndex.x;
+        //faces[0].SetLine(leftFaceTopLine, Direction.top);
+
+        //// Create and Set the right line
+        //Vector2Int leftFaceRightLine = new Vector2Int();
+        //leftFaceRightLine.x = verticesIndex.x;
+        //leftFaceRightLine.y = verticesIndex.y;
+        //faces[0].SetLine(leftFaceRightLine, Direction.right);
+
+        //// Create and Set the bottom line
+        //Vector2Int leftFaceBottomLine = new Vector2Int();
+        //leftFaceBottomLine.x = _lines[(int)Direction.bottom].x;
+        //leftFaceBottomLine.y = verticesIndex.y;
+        //faces[0].SetLine(leftFaceBottomLine, Direction.bottom);
+
+
+
+        //-------------------------------------------
+        // Create the right face
+        faces[1] = new Face(); // TODO: I have switched position between this face and the other one. This is supposed to be right face, but it is the left face....
+        int[] rightFaceVerticesIndex = new int[]
+        {
+            _verticesIndex[(int)VerticesPos.topLeft], // TopLeft
+            verticesIndex.x, // TopRight
+            verticesIndex.y, // BottomRight
+            _verticesIndex[(int)VerticesPos.bottomLeft] // BottomLeft
+        };
+
+        faces[1].SetVerticesIndex(rightFaceVerticesIndex);
+
+        return faces;
+    }
+
+
+    private Face[] _cutHorizontally(Vector2Int verticesIndex)
+    {
+        Face[] faces = new Face[2];
+
+        //-------------------------------------------
+        // Create the Left face
+        faces[0] = new Face();
+
+        int[] leftFaceVerticesIndex = new int[]
+        {
+            _verticesIndex[(int)VerticesPos.topLeft], // TopLeft
+            _verticesIndex[(int)VerticesPos.topRight], // TopRight
+            verticesIndex.y, // BottomRight
+            verticesIndex.x, // BottomLeft
+        };
+
+        faces[1].SetVerticesIndex(leftFaceVerticesIndex);
+
+
+
+        //-------------------------------------------
+        // Create the right face
+        faces[1] = new Face();
+        int[] rightFaceVerticesIndex = new int[]
+        {
+            verticesIndex.x, // TopLeft
+            verticesIndex.y, // TopRight
+            _verticesIndex[(int)VerticesPos.bottomRight], // BottomRight
+            _verticesIndex[(int)VerticesPos.bottomLeft] // BottomLeft
+        };
+
+        faces[1].SetVerticesIndex(rightFaceVerticesIndex);
+
+        return faces;
+    }
+}
