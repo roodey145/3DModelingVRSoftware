06214b590544abb9c7c3a1bd182f589c89cc84c7
nico386d <37042317+nico386d@users.noreply.github.com>
2023-05-09 12:44:54 +0200

    UI Text, Slider, Img.

--- Assets/Scripts/Sound/SoundUIController.cs
+++ Assets/Scripts/Sound/SoundUIController.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SoundUIController : MonoBehaviour
 {
     private Slider interactionSoundSlider;
     private Slider backgroundSoundSlider;
+    public TextMeshProUGUI interactionVolumeProcent;
+    public TextMeshProUGUI backgroundVolumeProcent;
+    public Image interactionImg;
+    public Image backgroundImg;
+    public Image[] muteUnmute;
+    
+
 
     private void Awake()
     {
@@ -14,33 +22,44 @@ public class SoundUIController : MonoBehaviour
         backgroundSoundSlider = GetComponent<Slider>();
         interactionSoundSlider.onValueChanged.AddListener(ChangeInteractionSoundVolume);
         backgroundSoundSlider.onValueChanged.AddListener(ChangeBackgroundSoundVolume);
+        interactionVolumeProcent.text = "Tool volume: " + interactionSoundSlider.value + "%";
+        backgroundVolumeProcent.text = "Envirement volume: " + backgroundSoundSlider.value + "%";
     }
 
     private void ChangeInteractionSoundVolume(float value)
     {
+        interactionVolumeProcent.text = "Tool volume: " + value + "%";
+
+
         SoundManager.InteractionSoundVolume = value;
 
         if (SoundManager.InteractionSoundVolume > 0f)
         {
             SoundManager.InteractionSoundPlaying = true;
+            interactionImg = muteUnmute[1];
         }
         else
         {
             SoundManager.InteractionSoundPlaying = !SoundManager.InteractionSoundPlaying;
+            interactionImg = muteUnmute[0];
         }
     }
 
     private void ChangeBackgroundSoundVolume(float value)
     {
+        backgroundVolumeProcent.text = "Envirement volume: " + value + "%";
+
         SoundManager.BackgroundSoundVolume = value;
 
         if (SoundManager.BackgroundSoundVolume > 0f)
         {
             SoundManager.BackgroundSoundPlaying = true;
+            backgroundImg = muteUnmute[1];
         }
         else
         {
             SoundManager.BackgroundSoundPlaying = !SoundManager.BackgroundSoundPlaying;
+            backgroundImg = muteUnmute[0];
         }
     }
 
@@ -48,10 +67,12 @@ public class SoundUIController : MonoBehaviour
     public void ToggleInteractionSound()
     {
         SoundManager.InteractionSoundPlaying = !SoundManager.InteractionSoundPlaying;
+        interactionImg = muteUnmute[0];
     }
 
     public void ToggleBackgroundSound()
     {
         SoundManager.BackgroundSoundPlaying = !SoundManager.BackgroundSoundPlaying;
+        backgroundImg = muteUnmute[0];
     }
 }
